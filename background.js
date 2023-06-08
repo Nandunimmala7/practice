bb90e32d9ee4e11967cb8c50912a2140b5bc52c9
nk <nimmala.nandakishore@gmailcom>
2023-06-07 22:26:10 -0400

    violet changed to viol

---
+++ background.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const color = ["red","blue",'green','yellow','white'];
+body.style.backgroundColor = 'viol';
+button.addEventListener('click', () => {
+    const colorIndex =  parseInt(Math.random()*color.length)
+    body.style.backgroundColor = color[colorIndex];
+})
