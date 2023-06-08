6291c52a0f5f23e8f16976d55d242ebcec44a049
nk <nimmala.nandakishore@gmailcom>
2023-06-07 22:34:13 -0400

    Revert "changed to violet"
    This reverts commit c231ac5283c344bd2f7cf35b5aa3dc6ee5bd5ec0.
    git going back to previous commit

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 const color = ["red","blue",'green','yellow','white'];
-body.style.backgroundColor = 'violet';
+body.style.backgroundColor = 'viol';
 button.addEventListener('click', () => {
     const colorIndex =  parseInt(Math.random()*color.length)
     body.style.backgroundColor = color[colorIndex];
